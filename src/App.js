1ebdb0dd51599e4c97245da8fe12225685742009
Ali Amin <aliamin745@gmail.com>
2023-07-02 16:56:39 +0300

    Packages installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Dashboard />} />
+				{/* <Route path="services" element={ <Services/> } /> */}
+				{/* <Route path="contact" element={ <Contact/> } /> */}
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
