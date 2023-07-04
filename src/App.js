a8c041da9501521644d2e3e228811cddf0fe77c1
Ali Amin <aliamin745@gmail.com>
2023-07-04 19:21:06 +0300

    Added firebase config

--- src/App.js
+++ src/App.js
@@ -3,18 +3,41 @@ import "./App.css";
 import Dashboard from "./pages/Dashboard";
 import VerticalNavbar from "./components/VerticalNavbar";
 
+import firebase from "firebase/compat/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import Header from "./components/Header";
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme";
+
+firebase.initializeApp({
+	apiKey: process.env.FB_KEY,
+	authDomain: "gamehub-dbb2b.firebaseapp.com",
+	projectId: "gamehub-dbb2b",
+	storageBucket: "gamehub-dbb2b.appspot.com",
+	messagingSenderId: "26190038291",
+	appId: "1:26190038291:web:9316c6a914159ba5d206e6",
+	measurementId: "G-6B7C5KJ51P",
+});
+
 function App() {
 	return (
-		<div className="App">
-			<VerticalNavbar />
-			<div className="ml-10 pl-10">
-				<Routes>
-					<Route path="/" element={<Dashboard />} />
-					{/* <Route path="services" element={ <Services/> } /> */}
-					{/* <Route path="contact" element={ <Contact/> } /> */}
-				</Routes>
+		<ThemeProvider theme={theme}>
+			<div className="App flex p-5 space-x-10">
+				<VerticalNavbar />
+				<div className=" flex flex-col gap-10 w-full">
+					<Header />
+					<Routes>
+						<Route path="/" element={<Dashboard />} />
+						{/* <Route path="services" element={ <Services/> } /> */}
+						{/* <Route path="contact" element={ <Contact/> } /> */}
+					</Routes>
+				</div>
 			</div>
-		</div>
+		</ThemeProvider>
 	);
 }
 
