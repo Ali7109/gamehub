015dec593a55b3af9e8830ef7d6de650e391c09e
Ali Amin <aliamin745@gmail.com>
2023-07-04 19:21:18 +0300

    Fixed positioning

--- src/components/VerticalNavbar.jsx
+++ src/components/VerticalNavbar.jsx
@@ -5,22 +5,22 @@ import { IconButton, Typography } from '@mui/material'
 
 const VerticalNavbar = () => {
   return (
-    <div className='fixed h-4/5 bg-gray-dark w-1/9 m-3 p-3 rounded-xl flex flex-col'>
+    <div className=' h-4/5 bg-gray-dark w-1/9 p-3 rounded-xl flex flex-col'>
         <div className="bg-orange h-10 w-10 rounded-full m-auto ">
             <Typography >Logo</Typography>
         </div> 
-        <div className="flex flex-col h-4/5 justify-around ">
+        <div className="flex flex-col h-4/5 justify-around" >
             <IconButton>
-                <FontAwesomeIcon className='m-auto p-2 rounded mt-10 text-lg text-gray-light transition hover:text-black hover:bg-orange' icon={faHouse} />
+                <FontAwesomeIcon className='m-auto p-2 rounded-xl mt-10 text-lg text-gray-light transition hover:text-black hover:bg-orange' icon={faHouse} />
             </IconButton>
             <IconButton >
-                <FontAwesomeIcon  className='m-auto p-2 rounded mt-5 text-lg text-gray-light transition hover:text-black hover:bg-orange'  icon={faMagnifyingGlass} />
+                <FontAwesomeIcon  className='m-auto p-2 rounded-xl mt-5 text-lg text-gray-light transition hover:text-black hover:bg-orange'  icon={faMagnifyingGlass} />
             </IconButton>
             <IconButton >
-                <FontAwesomeIcon className='m-auto p-2 rounded mt-5 text-lg text-gray-light transition hover:text-black hover:bg-orange' icon={faComments} />
+                <FontAwesomeIcon className='m-auto p-2 rounded-xl mt-5 text-lg text-gray-light transition hover:text-black hover:bg-orange' icon={faComments} />
             </IconButton>
             <IconButton >
-                <FontAwesomeIcon className='m-auto p-2 rounded mt-5 text-lg text-gray-light transition hover:text-black hover:bg-orange' icon={faShoppingCart} />
+                <FontAwesomeIcon className='m-auto p-2 rounded-xl mt-5 text-lg text-gray-light transition hover:text-black hover:bg-orange' icon={faShoppingCart} />
             </IconButton>
         </div>
     </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='ml-10 text-gray-light flex'>
-        <div className="m-10">Content</div>
+    
+        <div className="bg-gray-dark h-full max-h-72 w-full rounded-xl bg-dash-backdrop bg-center bg-cover">
+          {/* <img src={require('../images/gamingBackdrop.jpg')} alt="Image backdrop" /> */}
         </div>
   )
 }
