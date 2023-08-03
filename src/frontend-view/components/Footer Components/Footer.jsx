d9e75d0e60602e7063143389e31daed19d72eb8b
Ali Amin <aliamin745@gmail.com>
2023-08-03 18:40:15 +0300

    improved footer styling for mobile

--- src/frontend-view/components/Footer Components/Footer.jsx
+++ src/frontend-view/components/Footer Components/Footer.jsx
@@ -5,11 +5,13 @@ import PageList from './PageList';
 const Footer = () => {
   
   return (
-    <div className='w-full text-white text-xl font-yb bg-gray-dark rounded-t-xl h-44 p-10 flex items-center justify-evenly'>
-        <div className="rounded-full h-16 w-16">
-            <img src={require('../../images/ghlogo.png')} alt='Game hub logo'  className='rounded-full border border-black'/>
+    <div className='w-full text-white text-xl font-yb bg-gray-dark rounded-t-xl p-10 flex items-center justify-around'>
+        <div className="flex-col md:flex-row space-y-4 md:space-y-0">
+          <div className="rounded-full ">
+            <img src={require('../../images/ghlogo.png')} alt='Game hub logo'  className='rounded-full border border-black h-16 w-16'/>
+          </div>
+          <div className="">&copy;GameHub {new Date().getFullYear()}.<br></br>All rights reserved.</div>
         </div>
-        <div className="">&copy;GameHub {new Date().getFullYear()}. All rights reserved.</div>
         <div className="text-base">
             <PageList />
         </div>
