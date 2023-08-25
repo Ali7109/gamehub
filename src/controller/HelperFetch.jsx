eda55fe908c35b22b395cb2f01f4d99afda6278c
Ali Amin <aliamin745@gmail.com>
2023-08-25 20:52:55 +0300

    Fixed addReply and Moved file to controller

---
+++ src/controller/HelperFetch.jsx
@@ -0,0 +1,67 @@
+import { addDoc, arrayUnion, collection, doc, getDoc, getDocs, orderBy, query, serverTimestamp, setDoc, updateDoc } from "firebase/firestore"; 
+
+// Add a discussion to the specified game's discussionList subcollection
+
+export async function addDiscussion(db, gameId, content, userId, userName) {
+  const gameDocRef = doc(db, "discussions", ""+ gameId);
+  const discussionListRef = collection(gameDocRef, "discussionList");
+  
+  await addDoc(discussionListRef, {
+    content,
+    userId,
+    userName,
+    timestamp: serverTimestamp(),
+    replies: []
+  });
+}
+
+export async function addReply(db, gameId, discussionId, content, userId, userName) {
+  
+  const gameDocRef = doc(db, "discussions", "" + gameId);
+  const discussionRef = doc(gameDocRef, "discussionList", discussionId);
+  const timestamp = new Date(); // Generate timestamp on the client side
+  
+  // Update the discussion document to add a reply using arrayUnion
+  await updateDoc(discussionRef, {
+    replies: arrayUnion({ content, userId, userName, timestamp }),
+  });
+}
+
+// Fetch discussions for a specific game and sort by timestamp
+export async function fetchDiscussions(db, gameId) {
+  const gameDocRef = doc(db, "discussions", "" + gameId);
+  const discussionListRef = collection(gameDocRef, "discussionList");
+
+  const q = query(discussionListRef, orderBy("timestamp", "desc"));
+
+  const discussionSnapshot = await getDocs(q);
+
+  const discussions = [];
+  discussionSnapshot.forEach((doc) => {
+    discussions.push({
+      id: doc.id,
+      ...doc.data(),
+    });
+  });
+
+  return discussions;
+}
+
+
+// // Fetch discussions for a specific game
+// export async function fetchDiscussions(db, gameId) {
+//   const gameDocRef = doc(db, "discussions", ""+ gameId);
+//   const discussionListRef = collection(gameDocRef, "discussionList");
+  
+//   const discussionSnapshot = await getDocs(discussionListRef);
+  
+//   const discussions = [];
+//   discussionSnapshot.forEach((doc) => {
+//     discussions.push({
+//       id: doc.id,
+//       ...doc.data()
+//     });
+//   });
+
+//   return discussions;
+// }
