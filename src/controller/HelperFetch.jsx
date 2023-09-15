b39bc857f73875d6baf081b79f44607186855f0a
Ali Amin <aliamin745@gmail.com>
2023-09-14 23:11:08 -0400

    Removed unnecessary comments

--- src/controller/HelperFetch.jsx
+++ src/controller/HelperFetch.jsx
@@ -17,6 +17,7 @@ export async function addDiscussion(db, gameId, content, userId, userName) {
 
 
 
+
 export async function addReply(db, gameId, discussionId, content, userId, userName) {
   
   const gameDocRef = doc(db, "discussions", "" + gameId);
@@ -50,20 +51,3 @@ export async function fetchDiscussions(db, gameId) {
 }
 
 
-// // Fetch discussions for a specific game
-// export async function fetchDiscussions(db, gameId) {
-//   const gameDocRef = doc(db, "discussions", ""+ gameId);
-//   const discussionListRef = collection(gameDocRef, "discussionList");
-  
-//   const discussionSnapshot = await getDocs(discussionListRef);
-  
-//   const discussions = [];
-//   discussionSnapshot.forEach((doc) => {
-//     discussions.push({
-//       id: doc.id,
-//       ...doc.data()
-//     });
-//   });
-
-//   return discussions;
-// }
