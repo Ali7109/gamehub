d6e4815d654c9954e2f18f682abe2f2f06228b8e
Ali Hassan Amin <aliamin745@gmail.com>
2025-01-15 00:14:34 -0500

    BurgerMenu update

--- src/frontend-view/components/Nav Components/BurgerMenu.jsx
+++ src/frontend-view/components/Nav Components/BurgerMenu.jsx
@@ -1,18 +1,38 @@
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const BurgerMenu = ({ setCollapsed }) => {
+const BurgerMenu = ({ setCollapsed, collapsed }) => {
+	const [selected, setSelected] = useState(0);
+	const [menuOpen, setMenuOpen] = useState(false);
+
+	const handleClose = () => {
+		setMenuOpen(false);
+		setTimeout(() => {
+			setCollapsed(true);
+		}, 200);
+	};
+
+	useEffect(() => {
+		setTimeout(() => {
+			setMenuOpen(true);
+		}, 100);
+	}, [collapsed]);
 	const menu = [
 		{ path: "/", label: "Home" },
 		{ path: "/search", label: "Search" },
 		{ path: "/blog", label: "Blog" },
 	];
 	return (
-		<div className="md:hidden transition absolute top-0 h-64 w-full bg-gray-dark border-b-2 border-orange rounded-b-xl">
+		<div
+			className={
+				`z-10 md:hidden transition duration-500 -translate-y-full absolute top-0 pb-8 min-h-fit w-full bg-gray-dark border-b-2 border-orange rounded-b-xl ` +
+				(menuOpen ? "translate-y-0" : "")
+			}
+		>
 			<button
-				onClick={() => setCollapsed(true)}
+				onClick={() => handleClose()}
 				className="flex absolute h-8 w-8 rounded-full bottom-5 right-5 text-white transition hover:text-orange hover:bg-slate-600 "
 			>
 				<FontAwesomeIcon
@@ -22,8 +42,19 @@ const BurgerMenu = ({ setCollapsed }) => {
 			</button>
 			<ul className="m-5 mt-10 h-full flex-col justify-around text-white">
 				{menu.map((item, index) => (
-					<Link to={item.path} key={index}>
-						<li className="transition hover:border-orange hover:text-orange border-b-2 mb-5 pb-2 border-white">
+					<Link
+						to={item.path}
+						key={index}
+						onClick={() => setSelected(index)}
+					>
+						<li
+							className={
+								(index === selected
+									? "border-orange text-orange "
+									: "border-white ") +
+								` border-b-2 transition hover:border-orange hover:text-orange mb-5 pb-2`
+							}
+						>
 							{item.label}
 						</li>
 					</Link>
--- src/frontend-view/pages/Search.jsx
+++ src/frontend-view/pages/Search.jsx
@@ -1,137 +1,147 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import GameDataAPIController from '../../controller/GameDataAPIController'
-import GenreList from '../components/SearchPage Components/GenreList';
-import { CircularProgress, TextField } from '@mui/material';
-import { useSelector } from 'react-redux';
-import SearchCard from '../components/SearchPage Components/SearchCard';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import GameDataAPIController from "../../controller/GameDataAPIController";
+import GenreList from "../components/SearchPage Components/GenreList";
+import { CircularProgress, TextField } from "@mui/material";
+import { useSelector } from "react-redux";
+import SearchCard from "../components/SearchPage Components/SearchCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Search = () => {
+	const [loading, setloading] = useState(false);
 
-    const [loading, setloading] = useState(false);
+	const [genres, setGenres] = useState([]);
+	const [selectedGenres, setSelectedGenres] = useState([]);
+	const [shownTitles, setShownTitles] = useState({ results: [] });
+	const [search, setSearch] = useState("");
 
-    const [genres, setGenres] = useState([]);
-    const [selectedGenres, setSelectedGenres] = useState([]);
-    const [shownTitles, setShownTitles] = useState({results : []});
-    const [search, setSearch] = useState("");
-    
-    //Updates the selections as they are selected
-    const updatedSelections = (selected) => {
-      setSelectedGenres(selected)
-    }
+	//Updates the selections as they are selected
+	const updatedSelections = (selected) => {
+		setSelectedGenres(selected);
+	};
 
-    //Helper method to create the paramter part of the URL request
-    const createParameters = (selected) => {
-      let paramArr = "";
-      // console.log(selected)
-      let len = selected.length;
-      selected.map((selection, index) => {
-        paramArr += "" + selection.slug;
-        if(index + 1 !== len) paramArr += "," 
-        return true;
-      })
-      return paramArr;
-    }
+	//Helper method to create the paramter part of the URL request
+	const createParameters = (selected) => {
+		let paramArr = "";
+		// console.log(selected)
+		let len = selected.length;
+		selected.map((selection, index) => {
+			paramArr += "" + selection.slug;
+			if (index + 1 !== len) paramArr += ",";
+			return true;
+		});
+		return paramArr;
+	};
 
-    const handleSearchFilter = () => {
-      setloading(true)
-      let parameters = createParameters(selectedGenres);
-      console.log(search)
-      if(search.length !== 0) parameters += "&search=" +  search;
-      fetchFilteredSelection(parameters)
-      setloading(false)
-      setSearch("")
-      window.scrollTo({
-        top: 200,
-        behavior: 'smooth',
-      });
-    }
+	const handleSearchFilter = () => {
+		setloading(true);
+		let parameters = createParameters(selectedGenres);
+		console.log(search);
+		if (search.length !== 0) parameters += "&search=" + search;
+		fetchFilteredSelection(parameters);
+		setloading(false);
+		setSearch("");
+		window.scrollTo({
+			top: 200,
+			behavior: "smooth",
+		});
+	};
 
-    const fetchFilteredSelection = async (genres) => {
-      try {
-        let selections = "";
-        if(genres.substring(0, 1) === "&"){
-           selections = await GameDataAPIController("games",
-					`&page=1&page_size=20${genres}`)
-        } else {
-           selections = await GameDataAPIController("games",
-					`&page=1&page_size=20&genres=${genres}`)
-        }
-          setShownTitles(selections)
-          console.log(shownTitles)
-      } catch (error) {
-        console.error(error);
-      }
-    }
+	const fetchFilteredSelection = async (genres) => {
+		try {
+			let selections = "";
+			if (genres.substring(0, 1) === "&") {
+				selections = await GameDataAPIController(
+					"games",
+					`&page=1&page_size=20${genres}`
+				);
+			} else {
+				selections = await GameDataAPIController(
+					"games",
+					`&page=1&page_size=20&genres=${genres}`
+				);
+			}
+			setShownTitles(selections);
+			console.log(shownTitles);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-
-    //UseEffect to fetch data on first render
-    useEffect(() => {
-        const fetchGenres = async () => {
+	//UseEffect to fetch data on first render
+	useEffect(() => {
+		const fetchGenres = async () => {
 			try {
-            const genres = await GameDataAPIController("genres")
-			setGenres(genres);
+				const genres = await GameDataAPIController("genres");
+				setGenres(genres);
 			} catch (error) {
 				console.error(error);
 			}
 		};
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
-        fetchGenres()
-    }, [])
-  
-    
-  return (
-    <div className='rounded-xl w-full bg-gray-dark p-10 custom-scroll'>
-        <div className="w-full">
-          <TextField
-              InputProps={{
-                startAdornment: (
-                  <FontAwesomeIcon className='text-lg mr-3 text-gray-light' icon={faMagnifyingGlass} />
-                ),
-              }}
-              value={search}
-              onChange={e => {setSearch(e.target.value)}}
-              label="Search here!"
-              variant="standard"
-              color="warning"
-              className='w-full mb-3'
-          />
-        </div>
-        {genres.results ? 
-          <GenreList handleSearch={handleSearchFilter} genres={genres.results} updateSelections={updatedSelections}/>
-          :
-          <CircularProgress color='warning' className=' h-20'/>
-        }
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+		fetchGenres();
+	}, []);
 
-      {!loading ? 
-        shownTitles.results.length !== 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 p-3 h-96 overflow-scroll custom-scroll rounded-xl m-2 mt-10">
-          {shownTitles.results.map(title => (
-            <Link to={`/search/${title.id}`} key={title.id}>
-              <SearchCard title={title}/>
-            </Link>
-            ))}
-        </div> 
-            
-          ) : (
-            <div className="h-56 flex">
-              <h1 className='m-auto bg-gray text-white font-bold p-3 rounded-xl'>Search will show here</h1>
-            </div>
-          )
-          :
-          <div className="h-56 flex transition">
-            <CircularProgress color='warning' className='m-auto h-20'/>
-          </div>
-          }
-         
-    </div>
-  )
-}
+	return (
+		<div className="rounded-xl w-full bg-gray-dark p-10 custom-scroll">
+			<div className="w-full">
+				<TextField
+					InputProps={{
+						startAdornment: (
+							<FontAwesomeIcon
+								className="text-lg mr-3 text-gray-light"
+								icon={faMagnifyingGlass}
+							/>
+						),
+					}}
+					value={search}
+					onChange={(e) => {
+						setSearch(e.target.value);
+					}}
+					label="Search here!"
+					variant="standard"
+					color="warning"
+					className="w-full mb-3"
+				/>
+			</div>
+			{genres.results ? (
+				<GenreList
+					handleSearch={handleSearchFilter}
+					genres={genres.results}
+					updateSelections={updatedSelections}
+				/>
+			) : (
+				<CircularProgress color="warning" className=" h-20" />
+			)}
 
-export default Search
+			{!loading ? (
+				shownTitles.results.length !== 0 ? (
+					<div className="grid grid-cols-1 md:grid-cols-3 gap-5 p-3 h-96 overflow-scroll custom-scroll rounded-xl m-2 mt-10">
+						{shownTitles.results.map((title) => (
+							<Link to={`/search/${title.id}`} key={title.id}>
+								<SearchCard title={title} />
+							</Link>
+						))}
+					</div>
+				) : (
+					<div className="h-56 flex">
+						<h1 className="m-auto bg-gray text-white font-bold p-3 rounded-xl">
+							Search will show here
+						</h1>
+					</div>
+				)
+			) : (
+				<div className="h-56 flex transition">
+					<CircularProgress color="warning" className="m-auto h-20" />
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default Search;
